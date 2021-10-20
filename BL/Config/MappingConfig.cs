f7ee5255a6d8abe97d85c8f942b5eb57c31bbf1a
sladky <sladkyjan2@gmail.com>
2021-10-20 11:50:33 +0200

    Create some DTOs
     not all just what was done at lecture

---
+++ BL/Config/MappingConfig.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using BL.DTOs;
+using Domain.Entities;
+
+namespace BL.Config
+{
+    class MappingConfig
+    {
+        public static void ConfigureMapping(IMapperConfigurationExpression config)
+        {
+            config.CreateMap<User, UserGetDto>().ReverseMap();
+            config.CreateMap<User, UserPreviewDto>().ForMember(u => u.Photo, us => us.MapFrom(u => u.Photos.First()));
+            config.CreateMap<UserPhoto, UserPhotoGetDto>().ReverseMap();
+            config.CreateMap<Chat, ChatGetDto>().ReverseMap();
+        }
+    }
+}
