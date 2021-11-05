cedefd9baa4107677a312772640229dc466c712f
Marek Lassak <xlassak@fi.muni.cz>
2021-11-06 00:59:38 +0100

    added IGenericQuery and GenericQuery

---
+++ Domain/Interfaces/QueryInterfaces/IGenericQuery.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace Domain.Interfaces.QueryInterfaces
+{
+    public interface IGenericQuery<TEntity> where TEntity : class
+    {
+        void OrderBy<TKey>(Expression<Func<TEntity, TKey>> keySelector, bool isAscendingOrder = true);
+        void Page(int pageSize, int pageNumber);
+        Task<IEnumerable<TEntity>> ExecuteQueryAsync();
+    }
+}
---
+++ EFRepository/Queries/GenericQuery.cs
@@ -0,0 +1,38 @@
+﻿using Domain.Interfaces.QueryInterfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace EFInfrastructure.Queries
+{
+    public class GenericQuery<T> : IGenericQuery<T> where T : class
+    {
+        public IQueryable<T> CurrentResult { get; set; }
+
+        public async Task<IEnumerable<T>> ExecuteQueryAsync()
+        {
+            return await CurrentResult?.ToListAsync() ?? new List<T>();
+        }
+
+        public void OrderBy<TKey>(Expression<Func<T, TKey>> keySelector, bool isAscendingOrder = true)
+        {
+            if (isAscendingOrder)
+            {
+                CurrentResult.OrderBy(keySelector);
+            }
+            else
+            {
+                CurrentResult.OrderByDescending(keySelector);
+            }
+        }
+
+        public void Page(int pageSize, int pageNumber)
+        {
+            CurrentResult = CurrentResult.Skip(pageSize * (pageNumber - 1))
+                .Take(pageSize);
+        }
+    }
+}
