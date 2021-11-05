819dfc525db5ab0d9db4cb267d8f2f342280d7ab
Marek Lassak <xlassak@fi.muni.cz>
2021-11-05 22:03:49 +0100

    init migration

---
+++ EFRepository/Migrations/20211105210310_Initial.cs
@@ -0,0 +1,266 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EFInfrastructure.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    Surname = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    Birthdate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Gender = table.Column<int>(type: "int", nullable: false),
+                    Height = table.Column<int>(type: "int", nullable: false),
+                    Weight = table.Column<int>(type: "int", nullable: false),
+                    Bio = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: true),
+                    Longitude = table.Column<double>(type: "float", nullable: false),
+                    Latitude = table.Column<double>(type: "float", nullable: false),
+                    UserId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Users_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "FriendUsers",
+                columns: table => new
+                {
+                    UserId = table.Column<int>(type: "int", nullable: false),
+                    FriendId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FriendUsers", x => new { x.UserId, x.FriendId });
+                    table.ForeignKey(
+                        name: "FK_FriendUsers_Users_FriendId",
+                        column: x => x.FriendId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_FriendUsers_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Chats",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    MemberOneId = table.Column<int>(type: "int", nullable: false),
+                    MemberTwoId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Chats", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Chats_Users_MemberOneId",
+                        column: x => x.MemberOneId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Chats_Users_MemberTwoId",
+                        column: x => x.MemberTwoId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Preferences",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<int>(type: "int", nullable: false),
+                    MinAge = table.Column<int>(type: "int", nullable: false),
+                    MaxAge = table.Column<int>(type: "int", nullable: false),
+                    MinWeight = table.Column<int>(type: "int", nullable: false),
+                    MaxWeight = table.Column<int>(type: "int", nullable: false),
+                    MinHeight = table.Column<int>(type: "int", nullable: false),
+                    MaxHeight = table.Column<int>(type: "int", nullable: false),
+                    GpsRadius = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Preferences", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Preferences_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserPhotos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<int>(type: "int", nullable: false),
+                    Image = table.Column<byte[]>(type: "varbinary(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserPhotos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_UserPhotos_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Messages",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    AuthorId = table.Column<int>(type: "int", nullable: false),
+                    ChatId = table.Column<int>(type: "int", nullable: false),
+                    Text = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false),
+                    SendTime = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Messages", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Messages_Chats_ChatId",
+                        column: x => x.ChatId,
+                        principalTable: "Chats",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Messages_Users_AuthorId",
+                        column: x => x.AuthorId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "Bio", "Birthdate", "Gender", "Height", "Latitude", "Longitude", "Name", "Surname", "UserId", "Weight" },
+                values: new object[] { 1, "I am Franta.", new DateTime(2003, 11, 5, 21, 3, 9, 758, DateTimeKind.Utc).AddTicks(9419), 0, 200, 2.0, 1.0, "Franta", "Jahoda", null, 100 });
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "Bio", "Birthdate", "Gender", "Height", "Latitude", "Longitude", "Name", "Surname", "UserId", "Weight" },
+                values: new object[] { 2, "I am Frantiska.", new DateTime(2001, 11, 5, 21, 3, 9, 759, DateTimeKind.Utc).AddTicks(1548), 1, 150, 2.0, 1.0, "Frantiska", "Jahodova", null, 50 });
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "Bio", "Birthdate", "Gender", "Height", "Latitude", "Longitude", "Name", "Surname", "UserId", "Weight" },
+                values: new object[] { 3, "", new DateTime(1981, 11, 5, 21, 3, 9, 759, DateTimeKind.Utc).AddTicks(1563), 2, 185, 3.0, 2.0, "Petr", "Smutný", null, 94 });
+
+            migrationBuilder.InsertData(
+                table: "Chats",
+                columns: new[] { "Id", "MemberOneId", "MemberTwoId", "Name" },
+                values: new object[] { 1, 1, 2, "Our chat 1 a 2" });
+
+            migrationBuilder.InsertData(
+                table: "FriendUsers",
+                columns: new[] { "FriendId", "UserId" },
+                values: new object[] { 2, 1 });
+
+            migrationBuilder.InsertData(
+                table: "Preferences",
+                columns: new[] { "Id", "GpsRadius", "MaxAge", "MaxHeight", "MaxWeight", "MinAge", "MinHeight", "MinWeight", "UserId" },
+                values: new object[] { 1, 5, 25, 175, 90, 19, 150, 50, 1 });
+
+            migrationBuilder.InsertData(
+                table: "UserPhotos",
+                columns: new[] { "Id", "Image", "UserId" },
+                values: new object[] { 1, null, 1 });
+
+            migrationBuilder.InsertData(
+                table: "Messages",
+                columns: new[] { "Id", "AuthorId", "ChatId", "SendTime", "Text" },
+                values: new object[] { 1, 1, 1, new DateTime(2021, 11, 5, 21, 58, 9, 760, DateTimeKind.Local).AddTicks(5658), "Hello there" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FriendUsers_FriendId",
+                table: "FriendUsers",
+                column: "FriendId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Chats_MemberOneId",
+                table: "Chats",
+                column: "MemberOneId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Chats_MemberTwoId",
+                table: "Chats",
+                column: "MemberTwoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Messages_AuthorId",
+                table: "Messages",
+                column: "AuthorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Messages_ChatId",
+                table: "Messages",
+                column: "ChatId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Preferences_UserId",
+                table: "Preferences",
+                column: "UserId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserPhotos_UserId",
+                table: "UserPhotos",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_UserId",
+                table: "Users",
+                column: "UserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "FriendUsers");
+
+            migrationBuilder.DropTable(
+                name: "Messages");
+
+            migrationBuilder.DropTable(
+                name: "Preferences");
+
+            migrationBuilder.DropTable(
+                name: "UserPhotos");
+
+            migrationBuilder.DropTable(
+                name: "Chats");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
