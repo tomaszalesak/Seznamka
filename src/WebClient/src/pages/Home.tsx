16843dab9b4a8b9f67670c1ce896c48ac49c82ab
Tomáš Zálešák <zalesak.tomas@gmail.com>
2021-12-19 21:08:51 +0100

    remove home info

--- src/WebClient/src/pages/Home.tsx
+++ src/WebClient/src/pages/Home.tsx
@@ -1,9 +1,6 @@
 const Home = () => (
   <div>
-    <h1>Newsletter</h1>
-    <br />
     <h3>Welcome to the Dating site!</h3>
-    <div>As of 12/10/2021 the site has been deployed!</div>
   </div>
 );
 
